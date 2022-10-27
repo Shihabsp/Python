2bc02b6768129ed4e755b424f854d5bf9ed13fdc
Ayatullah Shihabi <60687835+Shihabsp@users.noreply.github.com>
2022-10-28 00:49:15 +0600

    Delete convert.py

--- convert.py
+++
@@ -1,9 +0,0 @@
-# convert the temperature in degree centigrade to fahrenheit
-#f=(9*c)/5+32
-def convert(c):
-    f = 9*c/5+32
-    return f
-    
-temperature = float(input("Enter temperature in centigrade: "))
-f = convert(temperature)
-print(f'{f} degree fahrenheit')
